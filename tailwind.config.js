b15cfedc38034153a38d75ee61d30e78b1a123e1
crazydumbbell <wogus5357@gmail.com>
2023-11-17 18:29:05 +0900

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{js,jsx,tx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
